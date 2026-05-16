c6aa930cdb43b03c5b51751667b9cdafa913cc76
Felipe Crisostomo Silva Oliveira <felipecrisostomo_@hotmail.com>
2026-05-15 23:36:43 -0300

    Passo 2 - Correções

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Projeto de Criação Aletaória de NPCs de RPG
 
 O objetivo deste projeto é aprimorar o aprendizado em Java através da automação da criação de Non-Player Characters (NPCs) para o sistema de RPG `D&D 5e`. O software utiliza as regras do `Guia do Mestre` para otimizar uma tarefa que, manualmente, demanda muito tempo com a verificação de 8 tabelas descritivas.
+
+### Melhorias a Serem Feitas
+
+1 - Tabela de Vínculo tem uma linha escrito "Role duas vezes, ignorando resultados iguais a 10", precisa ser tratado para rolar duas vezes automaticamente e devolver os dois resultados na linha.
+
+2 - Tabela de Origens precisa ser expandida, modificada ou repensada sobre seu uso.
--- src/Gerador.java
+++ src/Gerador.java
@@ -2,26 +2,58 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Gerador {
-    public static void main(String[] args) throws Exception {
+    public static NPC criarNpcAleatorio(String nome) {
         List<String> dadosAparencia = carregarArquivo("/tabelas/aparencia.txt");
+        List<String> dadosOrigem = carregarArquivo("/tabelas/origens/origem.txt");
         List<String> dadosHabAlta = carregarArquivo("/tabelas/habilidades/habilidadeAlta.txt");
+        List<String> dadosHabBaixa = carregarArquivo("/tabelas/habilidades/habilidadeBaixa.txt");
+        List<String> dadosDom = carregarArquivo("/tabelas/dom.txt");
+        List<String> dadosManeirismo = carregarArquivo("/tabelas/maneirismo.txt");
+        List<String> dadosTracos = carregarArquivo("/tabelas/tracoInterecao.txt");
+        List<String> dadosAlinhamento = carregarArquivo("/tabelas/ideais/alinhamento.txt");
+        List<String> dadosVinculos = carregarArquivo("/tabelas/vinculos.txt");
+        List<String> dadosSegredos = carregarArquivo("/tabelas/segredos.txt");
 
-        if (dadosAparencia == null ||
-            dadosHabAlta == null)  {
-                System.out.println("Houve erro de leitura, programa encerrado....");
-                return;
+        if (dadosAparencia == null || dadosHabAlta == null || dadosHabBaixa == null ||
+            dadosDom == null || dadosManeirismo == null || dadosTracos == null ||
+            dadosAlinhamento == null || dadosVinculos == null || dadosSegredos == null) {
+            System.out.println("Houve erro de leitura em uma das tabelas base. Cancelando...");
+            return null;
         }
 
-        Tabela tabelaAparencia = new Tabela(dadosAparencia);
-        Tabela tabelaHabAlta = new Tabela(dadosHabAlta);
+        String aparencia = new Tabela(dadosAparencia).rolar();
+        String origem = new Tabela(dadosOrigem).rolar();
+        String habAlta = new Tabela(dadosHabAlta).rolar();
+        String habBaixa = new Tabela(dadosHabBaixa).rolar();
+        String dom = new Tabela(dadosDom).rolar();
+        String maneirismo = new Tabela(dadosManeirismo).rolar();
+        String tracos = new Tabela(dadosTracos).rolar();
+        String vinculo = new Tabela(dadosVinculos).rolar();
+        String segredo = new Tabela(dadosSegredos).rolar();
 
-        System.out.println(tabelaAparencia.rolar());
-        System.out.println(tabelaHabAlta.rolar());
+        String alinhamentoSorteado = new Tabela(dadosAlinhamento).rolar();
+        List<String> tabelasElegiveis = new ArrayList<>();
+        tabelasElegiveis.add("/tabelas/ideais/outrosIdeais.txt"); 
 
+        if (alinhamentoSorteado.contains("Leal")) tabelasElegiveis.add("/tabelas/ideais/idealLeal.txt");
+        if (alinhamentoSorteado.contains("Caótico")) tabelasElegiveis.add("/tabelas/ideais/idealCaotico.txt");
+        if (alinhamentoSorteado.contains("Bom")) tabelasElegiveis.add("/tabelas/ideais/idealBom.txt");
+        if (alinhamentoSorteado.contains("Mau")) tabelasElegiveis.add("/tabelas/ideais/idealMau.txt");
+        if (alinhamentoSorteado.equals("Neutro")) tabelasElegiveis.add("/tabelas/ideais/idealNeutro.txt");
+
+        Tabela seletorDeTabela = new Tabela(tabelasElegiveis);
+        String caminhoTabelaEscolhida = seletorDeTabela.rolar();
+
+        List<String> dadosIdealEscolhido = carregarArquivo(caminhoTabelaEscolhida);
+        String idealSorteado = (dadosIdealEscolhido != null) ? new Tabela(dadosIdealEscolhido).rolar() : "Desconhecido";
+        String idealCompleto = alinhamentoSorteado + " (" + idealSorteado + ")";
+
+        return new NPC(nome, origem, aparencia, habAlta, habBaixa, dom, maneirismo, tracos, idealCompleto, vinculo, segredo);
     }
 
     public static List<String> carregarArquivo(String caminhoDoArquivo) {
